f557ad8e057f7915c6046a3d03b5a5d3782bbdca
Mark Brooks <Mark.x.brooks@gmail.com>
2025-08-26 10:35:59 -0400

    sending ribbon_group parameter to setup_ribbon_vbg

--- picogl/renderer/meshdata.py
+++ picogl/renderer/meshdata.py
@@ -14,12 +14,14 @@ class MeshData:
         nbo: np.ndarray = None,
         uvs: np.ndarray = None,
         cbo: np.ndarray = None,
+        ebo: np.ndarray = None,
     ):
         """set up the OpenGL context"""
         self.vbo = vbo
         self.nbo = nbo
         self.uvs = uvs
         self.cbo = cbo
+        self.ebo = ebo
 
         self.vertex_count = len(vbo.flatten()) // 3 if vbo is not None else None
 
@@ -121,25 +123,6 @@ class MeshData:
             nbo=nbo,
             uvs=uvs_arr,
             cbo=cbo_arr,
+            ebo=indices_arr
         )
 
-
-class MeshDataOld:
-    """Holds OpenGL-related state objects for rendering."""
-
-    def __init__(
-        self,
-        vbo: np.ndarray = None,
-        nbo: np.ndarray = None,
-        uvs: np.ndarray = None,
-        cbo: np.ndarray = None,
-    ):
-        """set up the OpenGL context"""
-        self.vbo = vbo
-        self.nbo = nbo
-        self.uvs = uvs
-        self.cbo = cbo
-        self.vertex_count = len(vbo.flatten()) // 3 if vbo is not None else None
-
-    def __str__(self):
-        return f"{self.vbo} {self.uvs} {self.cbo} "
