b16f5c3bf3ff35978f631cb69ad51e86ed1422f5
Mark Brooks <Mark.x.brooks@gmail.com>
2025-08-26 00:58:41 -0400

    adding context management to RibbonVBG

--- picogl/backend/legacy/core/vertex/buffer/color.py
+++ picogl/backend/legacy/core/vertex/buffer/color.py
@@ -25,18 +25,3 @@ class LegacyColorVBO(LegacyVBO):
         """Configure vertex attribute pointer for colors."""
         glColorPointer(self.size, self.dtype, self.stride, self.pointer)
 
-
-class LegacyColorVBOOld(LegacyVBO):
-    """Specialized Class for Color Buffers"""
-
-    def __init__(self, handle: int = None, data: np.ndarray = None, size: int = 3):
-        """constructor"""
-        super().__init__(handle=handle, size=size)
-        self.data = data
-        if data is not None:
-            self.set_data(data)
-        self.bind()
-
-    def configure(self):
-        """Configure attributes specific to color atoms_buffers"""
-        glColorPointer(self.size, self.dtype, self.stride, self.pointer)
