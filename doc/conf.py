8cade5ccb6ca532d3b656a6d34bcd1c2018a5a42
Brooks, Mark <mark.brooks@evotec.com>
2025-09-06 16:50:34 -0400

    shphinx pdf

--- doc/conf.py
+++ doc/conf.py
@@ -31,10 +31,11 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.rsvgconverter'
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Storqe']
 
 language = 'en'
 
