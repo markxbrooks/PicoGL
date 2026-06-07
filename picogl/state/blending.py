57ec32416576a7239b2a7f624afe78268936f6e1
Mark Brooks <Mark.x.brooks@gmail.com>
2026-06-07 09:56:18 -0400

    initial commit

--- picogl/state/blending.py
+++ picogl/state/blending.py
@@ -1,16 +1,22 @@
 from contextlib import contextmanager
 
 from OpenGL.GL import glIsEnabled
-from OpenGL.raw.GL.VERSION.GL_1_0 import GL_BLEND, GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA, glBlendFunc, glDisable, glEnable
+from OpenGL.raw.GL.VERSION.GL_1_0 import GL_BLEND, GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA, glBlendFunc, glDisable, \
+    glEnable, glGetIntegerv, GL_BLEND_SRC, GL_BLEND_DST
+
 
 @contextmanager
-def gl_blend():
+def gl_blend(src=GL_SRC_ALPHA, dst=GL_ONE_MINUS_SRC_ALPHA):
     was_enabled = glIsEnabled(GL_BLEND)
+    prev_src = glGetIntegerv(GL_BLEND_SRC)
+    prev_dst = glGetIntegerv(GL_BLEND_DST)
+
     try:
         if not was_enabled:
             glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glBlendFunc(src, dst)
         yield
     finally:
+        glBlendFunc(prev_src, prev_dst)
         if not was_enabled:
             glDisable(GL_BLEND)
