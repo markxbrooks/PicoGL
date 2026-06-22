3dc9ca01de8739305b777b1e48d4fd1d69ce9b3b
Brooks, Mark <mark.brooks@evotec.com>
2026-06-21 22:25:08 -0400

    optimizing arrow and ribbon

--- picogl/core/enums/index_type.py
+++ picogl/core/enums/index_type.py
@@ -11,7 +11,7 @@ from enum import IntEnum
 from OpenGL.raw.GL._types import GL_UNSIGNED_INT, GL_UNSIGNED_SHORT
 
 
-class GLIndexType(IntEnum)
+class GLIndexType(IntEnum):
     """Index type"""
     UNSIGNED_INT = GL_UNSIGNED_INT
     UNSIGNED_SHORT = GL_UNSIGNED_SHORT
