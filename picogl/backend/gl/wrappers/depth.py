91cb3106efe3e0d894833e3b20395be20edd0b89
Mark Brooks <mark.x.brooks@gmail.com>
2026-06-23 05:03:31 -0400

    Create depth.py

---
+++ picogl/backend/gl/wrappers/depth.py
@@ -0,0 +1,19 @@
+"""
+depth functions 
+"""
+
+from OpenGL.GL import glGetBooleanv
+from OpenGL.raw.GL.VERSION.GL_1_0 import (
+    GL_DEPTH_WRITEMASK,
+    glDepthMask,
+)
+from picogl.boolean import GLBoolean
+    
+    
+def gl_get_depth_mask():
+    return glGetBooleanv(GL_DEPTH_WRITEMASK)
+
+
+def gl_set_depth_mask(flag):
+    glDepthMask(GLBoolean.TRUE if flag else GLBoolean.FALSE)
+        
