c658a8a53e842a0ae51c4af168370aaaeb84718f
Mark Brooks <Mark.x.brooks@gmail.com>
2026-06-23 05:35:44 -0400

    initial commit

--- picogl/backend/gl/enums/data_type.py
+++ picogl/backend/gl/enums/data_type.py
@@ -7,7 +7,7 @@ solution for maintaining backward compatibility in scenarios where
 GLDataType is still referenced.
 """
 
-from picogl.backend.gl.enums import GLNumeric
+# from picogl.backend.gl.enums import GLNumeric
 
 """Compatibility shim"""
-GLDataType = GLNumeric  # Deprecated
+# GLDataType = GLNumeric  # Deprecated
