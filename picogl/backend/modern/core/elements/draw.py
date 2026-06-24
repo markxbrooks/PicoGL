1121ee19e87ef98812c19851bac946b9e6cf518c
Mark Brooks <mark.x.brooks@gmail.com>
2026-06-24 01:18:00 -0400

    Update draw.py

--- picogl/backend/modern/core/elements/draw.py
+++ picogl/backend/modern/core/elements/draw.py
@@ -1,12 +1,13 @@
 from OpenGL.raw.GL._types import GL_UNSIGNED_INT
 from OpenGL.raw.GL.VERSION.GL_1_0 import GL_LINES
 
+from picogl.backend.gl.enums import GLDrawMode, GLNumeric
 from picogl.backend.gl.wrappers import gl_draw_elements
 from picogl.backend.gl.wrappers.vertex_array import gl_bind_vertex_array
 
 
 def draw_elements(
-    vao: int, index_count: int, mode: int = GL_LINES, index_type: int = GL_UNSIGNED_INT
+    vao: int, index_count: int, mode: int = GLDrawMode.LINES, index_type: int = GLNumeric.UNSIGNED_INT
 ):
     """
     Helper method to bind a VAO and draw its elements.
