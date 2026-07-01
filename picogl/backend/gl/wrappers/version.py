3320fef49c51d95b231a37751f538ceac41d4088
Brooks, Mark <mark.brooks@evotec.com>
2026-07-01 18:34:24 -0400

    optimizing arrow and ribbon

---
+++ picogl/backend/gl/wrappers/version.py
@@ -0,0 +1,20 @@
+"""
+Retrieve the OpenGL version from the current context.
+
+This function utilizes the OpenGL library to fetch and return the current
+OpenGL version string, which provides information about the version of the
+OpenGL implementation in use.
+"""
+
+from OpenGL.raw.GL.VERSION.GL_1_0 import GL_VERSION
+
+from picogl.backend.gl.wrappers.string import gl_get_string
+
+
+def gl_get_version() -> str:
+    """gl get version"""
+    version = gl_get_string(GL_VERSION)
+    return version
+
+
+
--- picogl/info.py
+++ picogl/info.py
@@ -1,8 +1,9 @@
 """get gl information for printing"""
 
-from OpenGL.GL import glGetString
 from OpenGL.raw.GL.VERSION.GL_1_0 import GL_RENDERER, GL_VENDOR, GL_VERSION
 from OpenGL.raw.GL.VERSION.GL_2_0 import GL_SHADING_LANGUAGE_VERSION
+
+from picogl.backend.gl.wrappers.string import gl_get_string
 
 
 def get_gl_info():
@@ -13,9 +14,9 @@ def get_gl_info():
         OpenGL Version: {2}
         Shader Version: {3}
         """.format(
-        glGetString(GL_VENDOR).decode(),
-        glGetString(GL_RENDERER).decode(),
-        glGetString(GL_VERSION).decode(),
-        glGetString(GL_SHADING_LANGUAGE_VERSION).decode(),
+        gl_get_string(GL_VENDOR).decode(),
+        gl_get_string(GL_RENDERER).decode(),
+        gl_get_string(GL_VERSION).decode(),
+        gl_get_string(GL_SHADING_LANGUAGE_VERSION).decode(),
     )
     return info
