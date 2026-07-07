87d4573576920e3bb991a9eac03ab0da1885c60f
Mark Brooks <mark.x.brooks@gmail.com>
2026-07-07 17:44:07 -0400

    Add functions to enable, disable, and check capabilities
    Implement OpenGL capability enable/disable functions.

---
+++ picogl/backend/gl/wrappers/enable.py
@@ -0,0 +1,55 @@
+"""
+gl enable disable is enabled
+"""
+
+from typing import Union
+
+from OpenGL.raw.GL.VERSION.GL_1_0 import glDisable, glEnable, glIsEnabled
+from picogl.backend.gl.capability import GLFixedFunctionCapability, GLPipelineCapability
+from picogl.backend.gl.enums.point_size import (
+    GLLegacyPointCapability,
+    GLPointCapability,
+)
+from picogl.backend.gl.state.fill import GLCapability
+from picogl.texture.gltexture import GLTexture
+
+
+def gl_enable(
+    cap: Union[
+        GLCapability,
+        GLLegacyPointCapability,
+        GLPointCapability,
+        GLFixedFunctionCapability,
+        GLPipelineCapability,
+        GLTexture,
+    ],
+):
+    glEnable(cap)
+
+
+def gl_disable(
+    cap: Union[
+        GLCapability,
+        GLLegacyPointCapability,
+        GLPointCapability,
+        GLFixedFunctionCapability,
+        GLPipelineCapability,
+    ],
+):
+    glDisable(cap)
+
+
+def gl_is_enabled(
+    cap: Union[GLCapability, GLFixedFunctionCapability, GLPipelineCapability],
+):
+    return glIsEnabled(cap)
+
+
+def toggle_capability(
+    enabled: bool,
+    capability: Union[GLCapability, GLFixedFunctionCapability, GLPipelineCapability],
+):
+    if enabled:
+        gl_enable(capability)
+    else:
+        glDisable(capability)
