196c5d5d5294a3ba8494eba727bb5fdd173de163
Mark Brooks <mark.x.brooks@gmail.com>
2026-07-07 17:43:07 -0400

    Add gl_blend_func for OpenGL blending
    Implement gl_blend_func to set OpenGL blending function

---
+++ picogl/backend/gl/wrappers/blending.py
@@ -0,0 +1,12 @@
+"""
+blending
+"""
+
+from OpenGL.raw.GL.VERSION.GL_1_0 import glBlendFunc
+
+# from picogl.backend.gl.capability import GLBlendFactor
+
+
+def gl_blend_func(src: "GLBlendFactor", dst: "GLBlendFactor") -> None:
+    """gl blend func"""
+    glBlendFunc(src, dst)
