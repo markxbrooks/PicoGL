2314862dee1ca7c3d839be2b6cdba65ba25d976d
Brooks, Mark <mark.brooks@evotec.com>
2026-07-10 12:02:50 -0400

    optimizing arrow and ribbon

--- picogl/backend/gl/wrappers/matrix.py
+++ picogl/backend/gl/wrappers/matrix.py
@@ -1,6 +1,18 @@
+from __future__ import annotations
+
+from typing import Optional
+
+import numpy as np
 from OpenGL.raw.GL.VERSION.GL_1_0 import glMatrixMode
+from pyglm import glm
+
 from picogl.backend.gl.enums.legacy import GLLegacyMatrixMode
 
 
 def gl_matrix_mode(mode: GLLegacyMatrixMode):
     glMatrixMode(mode)
+
+
+def gl_load_matrixf(m: Optional[np.ndarray, glm.mat4]):
+    """gl load matrix"""
+    glLoadMatrixf(m)
