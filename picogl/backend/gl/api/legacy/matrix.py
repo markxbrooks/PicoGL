00a0a94cad901d70ce024a517a86213fea78ea58
Mark Brooks <Mark.x.brooks@gmail.com>
2026-07-13 07:49:38 -0400

    initial commit

--- picogl/backend/gl/api/legacy/matrix.py
+++ picogl/backend/gl/api/legacy/matrix.py
@@ -1,10 +1,11 @@
 from contextlib import contextmanager
 from OpenGL.GL import glPushMatrix, glPopMatrix
 
+
 @contextmanager
 def gl_pushed_matrix():
     """Push and automatically restore the current matrix."""
-    glPushMatrix()
+    glPushMatrix() # push the current matrix to the current stack
     try:
         yield
     finally:
---
+++ picogl/backend/gl/api/legacy/scale.py
@@ -0,0 +1,5 @@
+from OpenGL import GL as gl
+
+
+def gl_scale(x, y, z):
+    gl.glScale(x, y, z)
